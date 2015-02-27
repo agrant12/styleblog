c3cb254f882ad95727d113e79133951c1a23c546
agrant12 <alving.nyc@gmail.com>
2015-02-27 17:38:10 -0500

    Add about template page
    Update post category page

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: About Page
+ *
+ * This is the template that displays full width pages.
+ * 
+ * @package aThemes
+*/
+
+get_header(); ?>
+
+	<div id="content" class="site-content-wide" role="main">
+		<?php echo the_post_thumbnail('thumb-featured'); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content', 'single' ); ?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() )
+					comments_template();
+			?>
+
+		<?php endwhile; // end of the loop. ?>
+	</div>
+    
+<?php get_footer(); ?>
